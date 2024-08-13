59adda2e32f361d33797adeec67f5e844e97df6e
MojtabaaliabadiMoghadam <mojiland2002@gmail.com>
2024-08-13 21:06:04 +0330

    fix header style and hover that

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,12 @@ export default defineNuxtConfig({
         {name: 'viewport', content: 'width=device-width, initial-scale=1'}
       ],
       script: [],
-      link: [],
+      link: [
+        { 
+          rel: 'stylesheet', 
+          href: 'https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;700&display=swap' 
+        }
+      ],
       style: [],
       noscript: []
     },
