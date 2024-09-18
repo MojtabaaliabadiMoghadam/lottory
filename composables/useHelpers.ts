9aa961b140d7fa765dd3fb55d64b34457934f8bd
mohsenmehri6101 <mohsen.mehri6101@gmail.com>
2024-09-18 21:38:30 +0430

    ok

--- composables/useHelpers.ts
+++ composables/useHelpers.ts
@@ -114,7 +114,8 @@ export function useHelpers() {
     }
 
     function getUrl(url: string, api: boolean = true, hash_data: string | null = null): string {
-        const baseUrl = backEndUrl;
+        // const baseUrl = backEndUrl;
+        const baseUrl = 'https://ayandesabz.ir/';
         let normalizedUrl = url.replace(/\/+/g, '/');
         normalizedUrl = normalizedUrl.startsWith('/') ? normalizedUrl.slice(1) : normalizedUrl;
         normalizedUrl = api ? `api/${normalizedUrl}` : normalizedUrl;
