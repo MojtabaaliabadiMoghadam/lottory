e92ed377a998bf5547bcc1068623647629bac699
MojtabaaliabadiMoghadam <mojiland2002@gmail.com>
2024-10-08 23:49:51 +0330

    FEATURE || add page register

--- components/ui-kit/BaseButton.vue
+++ components/ui-kit/BaseButton.vue
@@ -2,7 +2,7 @@
   <div>
     <button @click="$emit('click-on')"
             class="bg-theme-primary-100  rounded-xl md:py-4 text-theme-primary-950 py-2 font-bold md:px-10 px-5
-                  hover:bg-theme-primary-900 hover:text-white transition-all duration-200 ease-in"
+                  hover:bg-theme-primary-700 hover:text-white transition-all duration-200 ease-in"
             :class="[props.classButton,props.active ?'!bg-theme-primary-900 !text-white':'']"
     >
       {{props.label}}
